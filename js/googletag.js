9f3e360e32f4ba5de394bb6deeddadafbfdd5236
cornerlotvisuals <cornerlotvisuals@gmail.com>
2024-02-21 11:50:06 +0800

    wip

--- js/googletag.js
+++ js/googletag.js
@@ -1,9 +1,6 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-MVCCPCPRD8"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+// Google tag (gtag.js)
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'G-MVCCPCPRD8');
-</script>
+gtag('config', 'G-MVCCPCPRD8');
