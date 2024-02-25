58e44a6b622fa8be1f049afe867be539f6ef717b
cornerlotvisuals <cornerlotvisuals@gmail.com>
2024-02-25 10:37:44 +0800

    wip

---
+++ js/404.js
@@ -0,0 +1,18 @@
+// Function to update the timer text
+        function updateTimer() {
+            var timerElement = document.getElementById('timer');
+            var seconds = 30;
+            var timer = setInterval(function() {
+                timerElement.textContent = seconds + ' seconds...';
+                seconds--;
+                if (seconds < 0) {
+                    clearInterval(timer);
+                    window.location.href = "index.html"; // Redirect to the home page
+                }
+            }, 1000);
+        }
+
+        // Call the updateTimer function when the page loads
+        window.onload = function() {
+            updateTimer();
+        };
