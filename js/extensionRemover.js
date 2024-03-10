eb79ce8d6410bcbd7049cc4ccf98545d7ba45402
cornerlotvisuals <cornerlotvisuals@gmail.com>
2024-03-10 11:01:24 +0800

    wip

---
+++ js/extensionRemover.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var url = window.location.href;
+    url = url.split('.html')[0];
+    window.history.replaceState( null, null, url );
+});
