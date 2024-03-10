b1ea7e75d0d77d4127779a9f8513768137459370
cornerlotvisuals <cornerlotvisuals@gmail.com>
2024-03-10 20:20:59 +0800

    wip

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,16 +1,24 @@
- // Add overscroll effects using JavaScript
-    document.body.addEventListener('wheel', function (e) {
-      if (document.body.scrollTop === 0 && e.deltaY > 0) {
-        // Overscroll at the top, add a bouncing effect
-        document.body.style.overscrollBehaviorY = 'auto';
-        setTimeout(function () {
-          document.body.style.overscrollBehaviorY = 'contain';
-        }, 100);
-      } else if (document.body.scrollHeight - window.innerHeight === document.body.scrollTop && e.deltaY < 0) {
-        // Overscroll at the bottom, add a bouncing effect
-        document.body.style.overscrollBehaviorY = 'auto';
-        setTimeout(function () {
-          document.body.style.overscrollBehaviorY = 'contain';
-        }, 100);
-      }
+document.body.addEventListener('wheel', function (e) {
+  handleOverscroll(e.deltaY);
+});
+
+document.body.addEventListener('mousewheel', function (e) {
+  handleOverscroll(e.deltaY);
+});
+
+function handleOverscroll(deltaY) {
+  if (document.body.scrollTop === 0 && deltaY > 0) {
+    // Overscroll at the top, add a bouncing effect
+    document.body.style.overscrollBehaviorY = 'auto';
+    setTimeout(function () {
+      document.body.style.overscrollBehaviorY = 'contain';
+    }, 100);
+  } else if (document.body.scrollHeight - window.innerHeight === document.body.scrollTop && deltaY < 0) {
+    // Overscroll at the bottom, add a bouncing effect
+    document.body.style.overscrollBehaviorY = 'auto';
+    setTimeout(function () {
+      document.body.style.overscrollBehaviorY = 'contain';
+    }, 100);
+  }
+}
     });
