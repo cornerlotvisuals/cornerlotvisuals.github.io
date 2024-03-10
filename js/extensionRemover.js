688ed08f423f29a6396afde38e869db8b0f32204
cornerlotvisuals <cornerlotvisuals@gmail.com>
2024-03-10 11:20:33 +0800

    wip

--- js/extensionRemover.js
+++ js/extensionRemover.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     var url = window.location.href;
-    // Remove '/pages' from the URL
-    var urlWithoutPages = url.replace('/pages', '');
     // Remove '/index' from the URL
     var urlWithoutIndex = urlWithoutPages.replace('/index', '');
     // Remove '.html' from the URL
