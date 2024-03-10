b6ef20f6594d25782a37292db653061f377d939d
cornerlotvisuals <cornerlotvisuals@gmail.com>
2024-03-10 11:07:05 +0800

    wip

--- js/extensionRemover.js
+++ js/extensionRemover.js
@@ -3,3 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
     url = url.split('.html')[0];
     window.history.replaceState( null, null, url );
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var url = window.location.href;
+    url = url.split('/index')[0];
+    window.history.replaceState( null, null, url );
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var url = window.location.href;
+    url = url.split('/pages')[0];
+    window.history.replaceState( null, null, url );
+});
